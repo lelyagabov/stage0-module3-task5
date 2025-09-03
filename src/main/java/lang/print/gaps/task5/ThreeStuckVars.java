d259947e7a886bc350ad21476d1d1db34d855e0d
lelyagabov <lelya_gabov@mail.ru>
2025-09-03 12:53:54 +0500

    task finished

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first = 1;
+        int second = 10;
+        int third = 100;
+
+        String empty = "";
+
+        System.out.println(first + empty + second + empty + third);
     }
 }
